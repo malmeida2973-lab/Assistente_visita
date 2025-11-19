5d3b3bc436b7d2c3040dc9548087d012bc42ae6e
malmeida2973-lab <malmeida2973@gmail.com>
2025-11-19 19:42:30 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-// PWA Service Worker (v25 - Assistente Externo Update)
-const CACHE_NAME = 'assistente-visita-cache-v25'; // <<< ATUALIZADO PARA v25
+const CACHE_NAME = 'assistente-visita-cache-v29'; // ATUALIZADO PARA v29
 const URLS_TO_CACHE = [
   '.',
   'index.html',
@@ -20,7 +19,6 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.filter(cacheName => {
-          // Apaga qualquer cache que comece com o prefixo mas não seja a v25
           return cacheName.startsWith('assistente-visita-cache-') && cacheName !== CACHE_NAME;
         }).map(cacheName => caches.delete(cacheName))
       );
@@ -29,7 +27,6 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  // Ignora pedidos para a API da Google ou outras APIs externas
   if (event.request.url.startsWith('http') && !event.request.url.includes('generativelanguage') && !event.request.url.includes('api.groq.com')) {
     event.respondWith(
       caches.open(CACHE_NAME).then(cache => {
